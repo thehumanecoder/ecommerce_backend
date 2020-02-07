30e2d6b313767e03253b0f34097f918e27e8cd49
Biswas Sampad <biswassampads@gmail.com>
2020-02-07 12:05:11 +0530

    store details access

--- api/routes/storeDetails.js
+++ api/routes/storeDetails.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const StoreDetails = require('../models/storeDetails');
+const ObjectId = require('mongodb').ObjectID;
 
-router.post('/', (req, res, next) => {
+router.post('/add', (req, res, next) => {
     const storeDetails = new StoreDetails({
         _id: new mongoose.Types.ObjectId(),
         storeid: req.body.storeid,
@@ -23,5 +24,29 @@ router.post('/', (req, res, next) => {
             console.log(err);
             res.status(500).json({ error: err });
         });
+});
+
+router.post('/:storeid',(req,res,next)=>{
+    var id = ObjectId(storeid)
+   StoreDetails.findOne({storeid:id},{'__v':0})
+   .exec()
+   .then(
+       doc=>{
+           console.log(doc);
+           if(doc != ''){
+               res.status(200).json(doc)
+           }else{
+               res.status(404).json({
+                   message:"There is no details present for the current store"
+               })
+           }
+       }
+   )
+   .catch()
+})
+
+router.post('/update/:storeid',(req,res,next)=>{
+    var id = ObjectId(storeid);
+    StoreDetails.findOne({storeid:id})
 });
 module.exports = router;
